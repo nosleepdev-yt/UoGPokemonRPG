6bac1468dbb57e3f93be048ee97911bd19d61e33
JeremiahWernham <31967734+JeremiahWernham@users.noreply.github.com>
2019-09-08 19:09:43 -0400

    name changes

--- PokemonUoG/src/dev/apexcanadian/pokemonuog/Launcher.java
+++
@@ -1,9 +0,0 @@
-package dev.apexcanadian.pokemonuog;
-
-public class Launcher {
-	
-	public static void main(String[] args) {
-		Game game = new Game("Pokemon UoG", 767, 639);
-		game.start();
-	}
-}
---
+++ PokemonUoG/src/dev/nosleepdev/pokemonuog/Launcher.java
@@ -0,0 +1,9 @@
+package dev.apexcanadian.pokemonuog;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		Game game = new Game("Pokemon UoG", 767, 639);
+		game.start();
+	}
+}
